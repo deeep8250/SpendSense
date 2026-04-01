643f85714a7897e95fba7577eb1afa533fda72eb
deeep8250 <deepmondal825005@gmail.com>
2026-04-01 19:09:25 +0530

    added unique constraints on years and months

--- Databases/migrations/00004_create_budget_up.sql
+++ Databases/migrations/00004_create_budget_up.sql
@@ -3,11 +3,11 @@ create table
         id serial primary key,
         user_id int references users (id) on delete cascade not null,
         category_id int references categories (id) on delete cascade not null,
-        amount float not null,
+        amount decimal(10,2) not null,
         month int not null,
         year int not null,
         created_at timestamp default now (),
-        unique (user_id, category_id)
+        unique (user_id, category_id,month,year)
     );
 
 create index if not exists idx_user_budget on budgets (user_id);
