4fdfc8e63cd5dd3f2299be112d170a11fe51e556
deeep8250 <deepmondal825005@gmail.com>
2026-04-01 20:51:07 +0530

    addeddb connection and try loop

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,50 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	// .env load
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatalf("Error loading .env: %v", err)
+	// }
+
+	DSN := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
+	var db *sqlx.DB
+	var err error
+
+	for range 5 {
+		db, err = sqlx.Connect("postgres", DSN)
+		if err != nil {
+			log.Println("trying to connect with database....")
+		}
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("database connection failed")
+
+	}
+
+	fmt.Println("database connect successfully")
+	// db connection
+
 	//dependency injection
 	r := gin.Default()
 	r.GET("/health", func(ctx *gin.Context) {
