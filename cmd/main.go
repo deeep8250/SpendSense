a30df447dd784588fc29b54a57f57cf9486abddb
deeep8250 <deepmondal825005@gmail.com>
2026-04-01 19:24:48 +0530

    initilize main

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	//dependency injection
+	r := gin.Default()
+	r.GET("/health", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"response": "server is running",
+		})
+
+	})
+	r.Run(":8080")
+
+}
