dcd59dbdf59e8c1099171ff1dfb6d3ca6b5d7460
deeep8250 <deepmondal825005@gmail.com>
2026-04-02 20:36:32 +0530

    added  2 second pause in db hit loop

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,11 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
+	handler "github.com/deeep8250/SpendSense/internal/handlers"
+	"github.com/deeep8250/SpendSense/internal/repositories"
+	"github.com/deeep8250/SpendSense/internal/services"
 	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
@@ -28,6 +32,8 @@ func main() {
 		db, err = sqlx.Connect("postgres", DSN)
 		if err != nil {
 			log.Println("trying to connect with database....")
+			//this time is important without that loop will quickly finished before db start
+			time.Sleep(2 * time.Second)
 		}
 	}
 
@@ -40,8 +46,11 @@ func main() {
 	fmt.Println("database connect successfully")
 
 	//dependency injection
+	AuthRepository := repositories.NewAuthRepository(db)
+	AuthService := services.NewAuthService(AuthRepository)
+	AuthHandler := handler.NewAuthHanler(AuthService)
 	r := gin.Default()
-	Routes(r)
+	Routes(r, AuthHandler)
 	r.Run(":8080")
 
 }
