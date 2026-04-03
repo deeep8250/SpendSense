88b4adb86d38d67cbb76d262e54598ccf6c39430
deeep8250 <deepmondal825005@gmail.com>
2026-04-03 07:58:09 +0530

    add duplicate error check in register

--- internal/services/auth_service.go
+++ internal/services/auth_service.go
@@ -1,6 +1,10 @@
 package services
 
 import (
+	"fmt"
+	"strings"
+
+	"github.com/deeep8250/SpendSense/auth/jwt"
 	"github.com/deeep8250/SpendSense/internal/repositories"
 	"github.com/deeep8250/SpendSense/models"
 	"golang.org/x/crypto/bcrypt"
@@ -31,8 +35,31 @@ func (s *AuthService) Register(userInput *models.Register) error {
 
 	err = s.repo.RegisterUser(user)
 	if err != nil {
+		if strings.Contains(err.Error(), "duplicate key") {
+			return fmt.Errorf("email already exists")
+		}
 		return err
 	}
 	return nil
+
+}
+
+func (s *AuthService) Login(userInput *models.Login) (string, error) {
+	user, err := s.repo.LoginUser(userInput)
+	if err != nil {
+		return "", err
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.HashedPassword), []byte(userInput.Password))
+	if err != nil {
+		return "", err
+	}
+
+	jwt, err := jwt.CreateJWT(user.Id)
+	if err != nil {
+		return "", nil
+	}
+
+	return jwt, nil
 
 }
