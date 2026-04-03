f186a92bc5bc1630f4134bf0e66b70f370d9c519
deeep8250 <deepmondal825005@gmail.com>
2026-04-03 20:59:33 +0530

    added routes and attach middleware for category and expenses

--- cmd/router.go
+++ cmd/router.go
@@ -3,11 +3,12 @@ package main
 import (
 	"net/http"
 
-	authHandler "github.com/deeep8250/SpendSense/internal/handlers"
+	"github.com/deeep8250/SpendSense/auth/middleware"
+	handler "github.com/deeep8250/SpendSense/internal/handlers"
 	"github.com/gin-gonic/gin"
 )
 
-func Routes(r *gin.Engine, authHandler *authHandler.AuthHandler) {
+func Routes(r *gin.Engine, authHandler *handler.AuthHandler, categoryHandler *handler.Categoryhandler, expenseHandler *handler.ExpenseHandler) {
 
 	r.GET("/health", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
@@ -16,6 +17,23 @@ func Routes(r *gin.Engine, authHandler *authHandler.AuthHandler) {
 
 	})
 
-	r.POST("/register", authHandler.Register)
-	r.POST("/login", authHandler.Login)
+	auth := r.Group("/auth")
+	{
+		auth.POST("/register", authHandler.Register)
+		auth.POST("/login", authHandler.Login)
+	}
+
+	protected := r.Group("/api")
+	protected.Use(middleware.Middleware())
+	//category
+	{
+		protected.GET("/category", categoryHandler.GetCategories)
+		protected.POST("/category", categoryHandler.CreateCategory)
+
+		// expenses
+		protected.GET("/expense", expenseHandler.GetAllExpensesHandler)
+		protected.GET("/expense/:id", expenseHandler.GetSingleExpenseHandler)
+		protected.POST("/expense", expenseHandler.CreateExpenseHandler)
+		protected.DELETE("/expense/:id", expenseHandler.DeleteExpenseHandler)
+	}
 }
